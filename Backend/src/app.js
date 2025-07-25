fd0db0b37a49a6f80d7f1db0ed97016e493d77bb
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-07-25 16:07:56 +0545

    written routes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import {connectToSocket} from "./controllers/socketManager.js";
-import userRouter from "./routes/user.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 
 const app = express();
@@ -17,6 +17,8 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
+
+app.use("/api/v1/users",userRoutes);
 
 // app.get("/home", (req, res) => {
 //   return res.json({ hello: "world" });
