0fa6dc35b3c22a3874de4a6bdd18e5b273d73054
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-08-04 00:22:02 +0545

    comment added

--- Backend/src/controllers/socketManager.js
+++ Backend/src/controllers/socketManager.js
@@ -6,20 +6,26 @@ let timeOnline={}
 
 export const connectToSocket = (server)=>{
     const io = new Server(server);
-
+  
     io.on("Connection",(socket)=>{
 
+        // join call or you can input any name you want
         socket.on("join-call",(path)=>{
             if(connections[path] === undefined){
                 connections[path] = []
             }
+
             connections[path].push(socket.id)
             timeOnline[socket.id] = new Date();
+
+            // notify exisiting users that a new user joined
             for(let a = 0; a < connections[path].length; a++){
                 io.to(connections[path][a].emit("user-joined",socket.id,connections[path]))
             }
+
             if(messages[path] !== undefined){
                 for(let a = 0; a < messages[path].length; ++a){
+                    // send chat history to the newly joined user
                     io.to(socket.id).emit("chat-message",messages[path][a]['data'],
                         messages[path][a]['sender'],messages[path][a]['socket-id-sender']
                     )
