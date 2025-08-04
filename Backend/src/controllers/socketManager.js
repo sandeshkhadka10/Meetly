cd51214c98f70859dd36023f61087b84df2fafbf
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-08-04 09:44:50 +0545

    added the coed for chat message exchange and disconnecting it

--- Backend/src/controllers/socketManager.js
+++ Backend/src/controllers/socketManager.js
@@ -12,15 +12,15 @@ export const connectToSocket = (server)=>{
         // join call or you can input any name you want
         socket.on("join-call",(path)=>{
             if(connections[path] === undefined){
-                connections[path] = []
+                connections[path] = [];
             }
 
-            connections[path].push(socket.id)
+            connections[path].push(socket.id);
             timeOnline[socket.id] = new Date();
 
             // notify exisiting users that a new user joined
             for(let a = 0; a < connections[path].length; a++){
-                io.to(connections[path][a].emit("user-joined",socket.id,connections[path]))
+                io.to(connections[path][a].emit("user-joined",socket.id,connections[path]));
             }
 
             if(messages[path] !== undefined){
@@ -31,19 +31,67 @@ export const connectToSocket = (server)=>{
                     )
                 }
             }
-        })
+        });
 
         socket.on("signal",(toId,message)=>{
+            // send chat history to the newly joined user
             io.to(toId).emit("signal",socket.id,message);
-        })
+        });
 
         socket.on("chat-message",(data,sender)=>{
+            // the main work of reduce here is to find the room name where the current socket is located.
+            const [matchingRoom, found] = Object.entries(connections)
+            .reduce(([room, isFound],[roomKey, roomValue]) =>{
+                if(!isFound && roomValue.includes(socket.id)){
+                    return [roomKey, true];
+                }
+                return [room, isFound];
+            },['',false]);
+            
+            // if matching room is found then only processed
+            if(found === true){
+                if(messages[matchingRoom] === undefined){
+                    messages[matchingRoom] = [];
+                }
+                messages[matchingRoom].push({"sender":sender, "data":data, "socket-id-sender":socket.id});
+                console.log("message",key,":",sender,data);
 
-        })
+                connections[matchingRoom].forEach((elem)=>{
+                    io.to(elem).emit("chat-message",data,sender,socket.id);
+                })
+            }
+        });
+
+        // to handle cleanup and notify others when a user disconnects and it runs automatically
         socket.on("disconnect",()=>{
+            var diffTime = Math.abs(timeOnline[socket.id] - new Date());
+            
+            // declares a variable to store the room name that this socket belonged to
+            var key;
+            
+            // key and value extract garera teslai check gareko
+            for(const[k,v] of JSON.parse(JSON.stringify(Object.entries(connections)))){
+                for(let a = 0; a < v.length; ++a){
+                    if(v[a] === socket.id){
+                        key = k;
+                        
+                        // tells every user in that room that the user has left
+                        for(let a = 0; a < connections[key].length; ++a){
+                            io.to(connections[key][a].emit("user-left",socket.id));
+                        }
+                        
+                        // find the position of the socket in the room list and remove using splice
+                        var index = connections[key].indexOf(socket.id);
+                        connections[key].splice(index,1);
 
-        })
-    })
+                        if(connections[key].length === 0){
+                            delete connections[key];
+                        }
+                    }
+                }
+            }
+        });
+    });
 
     return io;
 }
