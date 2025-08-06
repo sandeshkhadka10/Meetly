7bcdc589afe0d5da22829e7e968a4b718ad2c8f2
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-08-06 17:59:53 +0545

    controlling the audio and video

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -37,13 +37,15 @@ export default function VideoMeetComponent(){
 
     const getPermissions = async()=>{
         try{
+            // asking video permission
             const videoPermission = await navigator.mediaDevices.getUserMedia({video:true});
             if(videoPermission){
                 setVideoAvailable(true);
             }else{
                 setVideoAvailable(false);
             }
-
+            
+            // asking audio permission
             const audioPermission = await navigator.mediaDevices.getUserMedia({video:true});
             if(audioPermission){
                 setAudioAvailable(true);
@@ -51,8 +53,27 @@ export default function VideoMeetComponent(){
                 setAudioAvailable(false);
             }
             
+            // asking screen share permission
+            if(navigator.mediaDevices.getDisplayMedia){
+                setScreenAvailable(true);
+            }else{
+                setScreenAvailable(false);
+            }
+            
+            // if both video and audio is availabe
+            if(videoAvailable || audioAvailable){
+                const userMediaStream = await navigator.mediaDevices.getUserMedia({video: videoAvailable, audio: audioAvailable});
+        
+                if(userMediaStream){
+                    window.localStream = userMediaStream;
+                    if(localVideoRef.current){
+                        localVideoRef.current.srcObject = userMediaStream;
+                    }
+                }
+            }
+            
         }catch(error){
-
+            console.log(error);
         }
     }
 
@@ -64,6 +85,42 @@ export default function VideoMeetComponent(){
         getPermissions();
     },[])
 
+    let getUserMediaSuccess = (stream) => {
+
+    }
+
+    let getUserMedia = () =>{
+        if((video && videoAvailable) || (audio && audioAvailable)){
+            navigator.mediaDevices.getUserMedia({video: video, audio: audio})
+            .then(getUserMediaSuccess) // Todo: getUserMediaSuccess
+            .then((stream)=>{})
+            .catch((e)=>console.log(e))
+        }else{
+            try{
+                let tracks = localVideoRef.current.srcObject.getTracks();
+                tracks.forEach(track => track.stop())
+            }catch(error){
+
+            }
+        }
+    }
+    useEffect(() =>{
+        if(video !== undefined && audio !== undefined){
+            getUserMedia();
+        }
+    },[audio,video])
+
+    let getMedia = () =>{
+        setVideo(videoAvailable);
+        setAudio(audioAvailable);
+        // connectToSocketServer();
+    }
+
+    let connect = () =>{
+        setAskForUsername(false);
+        getMedia();
+    }
+
     return(
         <div>
             {/* main video features */}
@@ -72,7 +129,7 @@ export default function VideoMeetComponent(){
               <h2>Enter into lobby</h2>
               {username}
               <TextField id="outlined-basic" label="Username" value={username} onChange={(e)=>setUsername(e.target.value)} variant="outlined" />
-              <Button variant="contained">Connect</Button>
+              <Button variant="contained" onClick={connect}>Connect</Button>
               <div>
                 <video ref={localVideoRef}autoPlay muted></video>
               </div>
