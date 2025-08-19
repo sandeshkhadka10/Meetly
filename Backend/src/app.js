db5ef00e13c540fc79446bd875d3c3954e360230
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-08-19 22:57:54 +0545

    removed joi from global

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import {connectToSocket} from "./controllers/socketManager.js";
 import userRoutes from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use(cookieParser());
 
 app.use("/api/v1/users",userRoutes);
 
