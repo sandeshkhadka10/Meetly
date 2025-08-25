cbd33ff38b90a8991e47a11c91c86c9c0da543d7
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-08-25 22:40:07 +0545

    trying to display the error message of backend for form validation

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -32,9 +32,15 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { username, password } = req.body;
+
   if (!username && !password) {
-    return res.status(httpStatus.BAD_REQUEST).json({ message: "All fields are required" });
+    return res.status(httpStatus.BAD_REQUEST).json({ message: "Username and Password are required" });
+  }else if(!username){
+    return res.status(httpStatus.BAD_REQUEST).json({message:"Username is required"});
+  }else if(!password){
+    return res.status(httpStatus.BAD_REQUEST).json({message:"Password is required"});
   }
+  
   const user = await User.findOne({ username });
   if (!user) {
     return res.status(httpStatus.NOT_FOUND).json({ message: "User not found" });
