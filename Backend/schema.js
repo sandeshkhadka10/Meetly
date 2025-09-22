2a09df6ceb65cb99056363650c0c3be0bf38e3ea
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-09-22 12:35:42 +0545

    made the validation for the forget and reset password

--- Backend/schema.js
+++ Backend/schema.js
@@ -13,6 +13,18 @@ export const usersLoginSchema = Joi.object({
     password:Joi.string().required()
 });
 
+// for forget password
+export const forgetPasswordSchema = Joi.object({
+    email:Joi.string().required()
+});
+
+// for reset password
+export const resetPasswordSchema = Joi.object({
+    email:Joi.string().required(),
+    resetCode:Joi.string().required(),
+    newPassword:Joi.string().required()
+});
+
 // for meeting
 export const usersMeetingSchema = Joi.object({
     token:Joi.string().required(),
--- Backend/src/middlewares/validation.middleware.js
+++ Backend/src/middlewares/validation.middleware.js
@@ -1,5 +1,5 @@
 import ExpressError from "../util/ExpressError.js";
-import { usersRegisterSchema,usersLoginSchema, usersMeetingSchema } from "../../schema.js";
+import { usersRegisterSchema,usersLoginSchema,forgetPasswordSchema,resetPasswordSchema,usersMeetingSchema } from "../../schema.js";
 
 export const validateRegister = (req,res,next)=>{
     let {error} = usersRegisterSchema.validate(req.body);
@@ -13,6 +13,26 @@ export const validateRegister = (req,res,next)=>{
 
 export const validateLogin = (req,res,next)=>{
     let {error} = usersLoginSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+export const validateForgetPassword = (req,res,next)=>{
+    let {error} = forgetPasswordSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+export const validateResetPassword = (req,res,next)=>{
+    let {error} = resetPasswordSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el)=>el.message).join(",");
         throw new ExpressError(400,errMsg);
