e3c561ecead58bb652d773d8de34426987c181c6
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-09-28 14:50:48 +0545

    made the frontend part of forget password

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -3,18 +3,14 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AuthContext } from '../contexts/AuthContenxt';
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
 
 const defaultTheme = createTheme();
 
@@ -188,7 +184,7 @@ export default function Authentication() {
                 <div>
                   <span style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: "0.9rem" }}>
                     Forgot your password? {" "}
-                    <Link to="">Reset Here</Link>
+                    <Link to="/forgetPassword"> Reset Here</Link>
                   </span>
                 </div> : <></>}
 
